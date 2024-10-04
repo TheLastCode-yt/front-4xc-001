f5b97aadd2ad0032459227152790255e83d54412
The Last Code <thelastcodee@gmail.com>
2024-10-04 00:54:16 -0300

    add new component

--- src/components/InteractiveGallery.jsx
+++ src/components/InteractiveGallery.jsx
@@ -92,65 +92,62 @@ const InteractiveGallery = () => {
 
   return (
     <>
-      <div className="border-y-[0.5px] border-gray-200/20 bg-green-500">
-        <div className="container mx-auto">
-          <div className="w-full flex h-[520px] relative">
-            {items.map((item, index) => (
-              <div
-                key={index}
-                className={`w-full h-full flex-1 border-gray-200/20 group relative z-10
-                  ${index === 0 ? 'border-l-[0.5px]' : ''} 
+      <section className="py-10">
+        <div className="border-y-[0.5px] border-gray-200/70">
+          <div className="container mx-auto">
+            <div className="w-full flex h-[520px] relative">
+              {items.map((item, index) => (
+                <div
+                  key={index}
+                  className={`w-full h-full flex-1 border-gray-200/70 group relative 
+                ${index === 0 ? 'border-l-[0.5px]' : ''} 
                 ${
                   index === items.length - 1
                     ? 'border-x-[0.5px]'
                     : 'border-l-[0.5px]'
                 }
-              
-               
-                 `}
-                onClick={() => handleSelectItem(index)}
-              >
-                <button
-                  className={`w-full  h-full inline-flex items-start justify-center group-hover:border-t-custom__blue 
-                  border-t-[3px] border-t-transparent font-semibold text-sm
-                 transition-all duration-300 pt-[4%]  text-custom__blue group-hover:bg-[#0056ff]/5 group-hover:backdrop-blur-sm group-hover:text-black
-                ${
-                  activateItem.title === item.title
-                    ? 'border-t-custom__blue bg-[#0056ff]/5 backdrop-blur-sm'
-                    : ''
-                }
-                `}
+              `}
+                  onClick={() => handleSelectItem(index)}
                 >
-                  {item.title}
-                </button>
-              </div>
-            ))}
+                  <button
+                    className={`w-full h-full inline-flex items-start justify-center group-hover:border-t-custom__blue 
+                  border-t-[3px] border-t-transparent font-semibold text-sm
+                  transition-all duration-300 pt-[4%] text-custom__blue group-hover:bg-[#0056ff]/5 
+                  group-hover:backdrop-blur-sm group-hover:text-black
+                  ${
+                    activateItem.title === item.title
+                      ? 'border-t-custom__blue bg-[#0056ff]/5 backdrop-blur-sm'
+                      : ''
+                  }
+                `}
+                    aria-pressed={activateItem.title === item.title}
+                  >
+                    {item.title}
+                  </button>
+                </div>
+              ))}
 
-            <div className="absolute flex items-center pl-[4%] w-full h-full text-black">
-              <div className="flex flex-col gap-y-4 max-w-[26%]">
-                <h3 className="text-5xl font-bold">{activateItem.title}</h3>
-                <p className="text-xl">{activateItem.text}</p>
-                <ButtonOrLink className={'bg-custom__blue z-10'}>
-                  {activateItem.btnText}
-                </ButtonOrLink>
-              </div>
+              <Image
+                src={activateItem.imageUrl}
+                alt={activateItem.title}
+                width={686}
+                height={520}
+                className="object-contain max-h-[520px] absolute -z-10 transition-opacity duration-300 top-[60px] bottom-0 right-0"
+              />
 
-              <div className="w-full h-full bg-rose-500 relative">
-                <div className="absolute top-0 left-0">
-                  <Image
-                    // width={686}
-                    // height={520}
-                    src={activateItem.imageUrl}
-                    alt={activateItem.title}
-                    fill
-                    className="object-contain max-h-[520px] absolute"
-                  />
+              <div className="absolute flex items-center pl-[4%] w-full h-full text-black pointer-events-none">
+                <div className="flex flex-col gap-y-4 max-w-[26%]">
+                  <h3 className="text-5xl font-bold">{activateItem.title}</h3>
+                  <p className="text-xl">{activateItem.text}</p>
+                  <ButtonOrLink className={'bg-custom__blue z-10'}>
+                    {activateItem.btnText}
+                  </ButtonOrLink>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
