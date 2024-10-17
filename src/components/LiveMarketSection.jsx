315927ae544c27b0b3225a9707029631203ba5f8
The Last Code <thelastcodee@gmail.com>
2024-10-17 13:29:26 -0300

    create responsivy live market section

--- src/components/LiveMarketSection.jsx
+++ src/components/LiveMarketSection.jsx
@@ -40,15 +40,15 @@ const LiveMarketSection = () => {
   // Função para renderizar os dados conforme a opção selecionada
 
   return (
-    <section className="text-white -mt-[15%]">
+    <section className="text-white -mt-[40%] xl:-mt-[15%] bg-black">
       <div className="container mx-auto p-4">
         {/* <button onClick={() => console.log(stocksData.data)}>
           console.log
         </button> */}
 
-        <div className="flex flex-col gap-y-4">
+        <div className="flex flex-col xl:gap-y-4">
           {/* Botões de seleção */}
-          <div className="flex items-center justify-center gap-x-20 mb-6">
+          <div className="flex items-center xl:justify-center xl:gap-x-20 mb-6  overflow-hidden overflow-x-scroll">
             {options.map(option => (
               <button
                 key={option.value}
@@ -145,11 +145,11 @@ const Table = ({ selectedOption, sectionTitle, sectionLink, data }) => {
   return (
     <div className="w-full flex flex-col bg-black text-white rounded shadow shadow-white">
       {/* header */}
-      <div className="grid grid-cols-12 items-center py-6 px-4 bg-custom__blue-focus">
-        <div className="col-span-4 flex items-start">
+      <div className="grid grid-cols-6 xl:grid-cols-12 items-center py-6 px-4 bg-custom__blue-focus xl:gap-y-0 gap-y-4">
+        <div className="col-span-6 xl:col-span-4 flex items-start">
           <Link
             href={sectionLink}
-            className="flex items-center text-4xl gap-x-2 justify-center"
+            className="flex items-center text-xl xl:text-4xl gap-x-2 justify-center"
           >
             {sectionTitle}
             <Image
@@ -157,12 +157,12 @@ const Table = ({ selectedOption, sectionTitle, sectionLink, data }) => {
               width={20}
               height={20}
               alt="chevron icon"
-              className="invert"
+              className="invert w-[14px] h-[14px] xl:w-[20px] xl:h-[20px]"
             />
           </Link>
         </div>
 
-        <div className="col-span-2">
+        <div className="col-span-2 xl:block hidden">
           <span>
             {selectedOption === 'forex' || selectedOption === 'crypto'
               ? 'Bid'
@@ -170,7 +170,7 @@ const Table = ({ selectedOption, sectionTitle, sectionLink, data }) => {
           </span>
         </div>
 
-        <div className="col-span-2">
+        <div className="col-span-2 xl:block hidden">
           <span>
             {selectedOption === 'forex' || selectedOption === 'crypto'
               ? 'Ask'
@@ -178,7 +178,7 @@ const Table = ({ selectedOption, sectionTitle, sectionLink, data }) => {
           </span>
         </div>
 
-        <div className="col-span-2">
+        <div className="col-span-2 xl:block hidden">
           <span>
             {selectedOption === 'forex' || selectedOption === 'crypto'
               ? '1D change'
@@ -186,7 +186,18 @@ const Table = ({ selectedOption, sectionTitle, sectionLink, data }) => {
           </span>
         </div>
 
-        <div className="col-span-2 h-10"></div>
+        <div className="col-span-2 xl:h-10 xl:block hidden"></div>
+
+        {/* mobile options */}
+        <div className=" col-span-3 xl:hidden">
+          <span className="font-semibold text-lg">$10</span>
+          <p className="text-xs">Depósito mín.</p>
+        </div>
+
+        <div className=" col-span-3 xl:hidden">
+          <span className="font-semibold text-lg">$1</span>
+          <p className="text-xs">Investimento mínimo</p>
+        </div>
       </div>
 
       {/* grids para forex e crypto */}
@@ -201,15 +212,16 @@ const Table = ({ selectedOption, sectionTitle, sectionLink, data }) => {
             return (
               <div
                 key={`${index}+image-logo-active`}
-                className="p-2 grid grid-cols-12 items-center border-b-gray-200/10 border-b-[0.1px] last:border-b-transparent"
+                className="p-2 grid grid-cols-6 xl:grid-cols-12 items-center border-b-gray-200/10 border-b-[0.1px] last:border-b-transparent"
               >
                 {/* Nome e Logo */}
-                <div className="flex items-center justify-start gap-x-2 col-span-4">
+                <div className="flex items-center justify-start gap-x-2 col-span-4 xl:col-span-4">
                   <Image
                     src={item.image}
                     width={50}
                     height={50}
                     alt="logo assets active"
+                    className="w-[40px] h-[40px] xl:h-[50px] xl:w-[50px]"
                   />
                   <div className="flex flex-col">
                     <p className="text-sm font-semibold">{item.title}</p>
@@ -220,29 +232,41 @@ const Table = ({ selectedOption, sectionTitle, sectionLink, data }) => {
                 </div>
 
                 {/* Bid */}
-                <div className="col-span-2">{bidPrice.toFixed(6)}</div>
+                <div className="col-span-2  hidden xl:block">
+                  {bidPrice.toFixed(6)}
+                </div>
 
                 {/* Ask */}
-                <div className="col-span-2">{askPrice.toFixed(6)}</div>
+                <div className="col-span-2  hidden xl:block">
+                  {askPrice.toFixed(6)}
+                </div>
 
                 {/* Diferença % */}
                 <div
-                  className={`col-span-2 font-semibold ${
-                    priceDifferencePercent > 0
-                      ? 'text-green-500'
-                      : 'text-red-500'
-                  }`}
+                  className={`col-span-2 font-semibold  hidden xl:block
+                    ${
+                      priceDifferencePercent > 0
+                        ? 'text-green-500'
+                        : 'text-red-500'
+                    }`}
                 >
                   {priceDifferencePercent.toFixed(2)}%
                 </div>
 
                 {/* Botões */}
-                <div className="flex gap-x-2 col-span-2 justify-end">
+                <div className=" hidden xl:flex gap-x-2 col-span-2 justify-end">
                   <button className="py-[10px] px-[20px] flex items-center overflow-hidden relative text-red-500 border border-red-500 hover:text-white hover:bg-red-500 transition-all">
                     Vender
                   </button>
                   <button className="py-[10px] px-[20px] flex items-center overflow-hidden relative text-green-500 hover:text-white hover:bg-green-500 border border-green-500 transition-all">
                     Comprar
+                  </button>
+                </div>
+
+                {/* botoes mobile */}
+                <div className="block xl:hidden col-span-2 ml-auto">
+                  <button className="py-[10px] px-[20px] flex items-center justify-center overflow-hidden relative text-red-500 border border-red-500 hover:text-white hover:bg-red-500 transition-all text-xs w-[107px] text-center">
+                    {askPrice.toFixed(6)}
                   </button>
                 </div>
               </div>
@@ -267,15 +291,16 @@ const Table = ({ selectedOption, sectionTitle, sectionLink, data }) => {
             return (
               <div
                 key={`${index}+image-logo-inactive`}
-                className="p-2 grid grid-cols-12 items-center border-b-gray-200/10 border-b-[0.1px] last:border-b-transparent"
+                className="p-2 grid grid-cols-6 xl:grid-cols-12 items-center border-b-gray-200/10 border-b-[0.1px] last:border-b-transparent"
               >
                 {/* Nome e Logo */}
-                <div className="flex items-center justify-start gap-x-2 col-span-4">
+                <div className="flex items-center justify-start gap-x-2 col-span-4 xl:col-span-4">
                   <Image
                     src={item.image}
                     width={50}
                     height={50}
                     alt="logo assets inactive"
+                    className="w-[40px] h-[40px] xl:h-[50px] xl:w-[50px]"
                   />
                   <div className="flex flex-col">
                     <p className="text-sm font-semibold">{item.title}</p>
@@ -286,29 +311,41 @@ const Table = ({ selectedOption, sectionTitle, sectionLink, data }) => {
                 </div>
 
                 {/* Low */}
-                <div className="col-span-2">{lowPrice.toFixed(6)}</div>
+                <div className="col-span-2 xl:block hidden">
+                  {lowPrice.toFixed(6)}
+                </div>
 
                 {/* High */}
-                <div className="col-span-2">{highPrice.toFixed(6)}</div>
+                <div className="col-span-2 xl:block hidden">
+                  {highPrice.toFixed(6)}
+                </div>
 
                 {/* Variação % */}
                 <div
-                  className={`col-span-2 font-semibold ${
-                    priceDifferencePercent > 0
-                      ? 'text-green-500'
-                      : 'text-red-500'
-                  }`}
+                  className={`col-span-2 font-semibold xl:block hidden
+                    ${
+                      priceDifferencePercent > 0
+                        ? 'text-green-500'
+                        : 'text-red-500'
+                    }`}
                 >
                   {priceDifferencePercent.toFixed(2)}%
                 </div>
 
                 {/* Botões */}
-                <div className="flex gap-x-2 col-span-2 justify-end">
+                <div className="hidden xl:flex gap-x-2 col-span-2 justify-end ">
                   <button className="py-[10px] px-[20px] flex items-center overflow-hidden relative text-red-500 border border-red-500 hover:text-white hover:bg-red-500 transition-all">
                     Vender
                   </button>
                   <button className="py-[10px] px-[20px] flex items-center overflow-hidden relative text-green-500 hover:text-white hover:bg-green-500 border border-green-500 transition-all">
                     Comprar
+                  </button>
+                </div>
+
+                {/* botoes mobile */}
+                <div className="block xl:hidden col-span-2 ml-auto">
+                  <button className="py-[10px] px-[20px] flex items-center justify-center overflow-hidden relative text-red-500 border border-red-500 hover:text-white hover:bg-red-500 transition-all text-xs w-[107px] text-center">
+                    {highPrice.toFixed(6)}
                   </button>
                 </div>
               </div>
